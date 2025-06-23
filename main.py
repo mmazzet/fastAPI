81d00f37fe46bf730f671a1d6083b4b201ecfc37
Marianna <20104120@mail.wit.ie>
2025-06-23 22:40:16 +0100

    update status code to created; add http exception not found

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -29,7 +29,7 @@ def root():
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 100000000000)
@@ -37,8 +37,15 @@ def create_posts(post:Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int ):
-    print(type(id))
+def get_post(id: int, response:  Response):
+    
     post = find_post(id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Post with id: {id} was not found')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message': f'Post with id: {id} was not found'}
     print(post)
     return {"post_detail": post}
+
