bdd2dcad98c72413829ca8b85bae7978a7d9bff3
Marianna <20104120@mail.wit.ie>
2025-07-13 18:56:13 +0100

    add models and sqlalchemy for db mng

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
 
 
 class Post(BaseModel):
@@ -51,6 +58,10 @@ def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p["id"] == id:
             return i
+
+@app.get("/sqlalchemy")
+def test_post(db:Session = Depends(get_db)):
+    return {"status": "success"}
 
 
 @app.get("/")
