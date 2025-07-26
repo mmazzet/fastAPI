a8272d234efa153217228352db24ec7bdfa4ce1f
Marianna <20104120@mail.wit.ie>
2025-07-26 16:21:53 +0100

    get user from protected route

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,14 @@
-from jose import JWSError, jwt
+from jose import ExpiredSignatureError, JWSError, jwt
 from datetime import datetime, timedelta
+from fastapi import Depends, status, HTTPException
 
 from dotenv import load_dotenv
 import os
+from . import schemas, database, models
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 load_dotenv()
 
@@ -13,10 +19,36 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=str(id))
+    except ExpiredSignatureError:
+        raise credentials_exception
+    except JWSError:
+        raise credentials_exception
+    
+    return token_data
+    
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
+
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
 
