c7627f6a486bfb141adc4030db951edd62245cc9
Marianna <20104120@mail.wit.ie>
2025-07-26 22:52:30 +0100

    update and delete own posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,25 +3,29 @@ from typing import List
 from .. import models, schemas
 from sqlalchemy.orm import Session
 from .. database import get_db
-from .. import  oauth2
+from .. import oauth2
 
-router = APIRouter(
-    prefix='/posts',
-    tags=['Posts']
-)
+router = APIRouter(prefix="/posts", tags=["Posts"])
+
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int =  Depends(oauth2.get_current_user)):
+def get_posts(
+    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
+):
     posts = db.query(models.Post).all()
     return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db:Session = Depends(get_db), current_user: int =  Depends(oauth2.get_current_user)):
-#      cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
-#                    (post.title, post.content, post.published))
-#     new_post = cursor.fetchone()
-#     conn.commit()
+def create_posts(
+    post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
+    #      cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #                    (post.title, post.content, post.published))
+    #     new_post = cursor.fetchone()
+    #     conn.commit()
 
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
@@ -31,7 +35,11 @@ def create_posts(post: schemas.PostCreate, db:Session = Depends(get_db), current
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db), current_user: int =  Depends(oauth2.get_current_user)):
+def get_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id)))
     # post = cursor.fetchone()
 
@@ -47,26 +55,42 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int =  Depend
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), current_user: int =  Depends(oauth2.get_current_user)):
+def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s returning * """, (str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    
-    if post.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+    if post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {id} does not exist",
         )
 
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db:Session = Depends(get_db), current_user: int =  Depends(oauth2.get_current_user)):
+def update_post(
+    id: int,
+    updated_post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
@@ -77,6 +101,12 @@ def update_post(id: int, updated_post: schemas.PostCreate, db:Session = Depends(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {id} does not exist",
+        )
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
         )
 
     post_query.update(updated_post.dict(), synchronize_session=False)
