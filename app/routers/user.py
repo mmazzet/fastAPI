3c9cba39d203d9788e2eb22946f3d3599ebadd66
Marianna <20104120@mail.wit.ie>
2025-07-26 18:45:28 +0100

    check if email already exists

--- app/routers/user.py
+++ app/routers/user.py
@@ -13,6 +13,13 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db:Session = Depends(get_db)):
 
+    # Check if the username already exists
+    existing_user = db.query(models.User).filter(models.User.email== user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="This email already exists so I gave you a great hack info",
+        )
 
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
