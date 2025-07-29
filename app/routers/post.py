fb3ede973afa4d5fa4d48973e7b2fb4072ad5b96
Marianna <20104120@mail.wit.ie>
2025-07-29 21:56:56 +0100

    add query parameters for get posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-from typing import List
+from typing import List, Optional
 from .. import models, schemas
 from sqlalchemy.orm import Session
 from .. database import get_db
@@ -10,14 +10,18 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(
-    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
+    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+    limit: int = 10,
+    skip: int = 0,
+    search: Optional[str] = ""
 ):
+    print(limit)
 
 
     # Enables the owner to see only own posts
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
-
-    posts = db.query(models.Post).all()
+    print(search)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
