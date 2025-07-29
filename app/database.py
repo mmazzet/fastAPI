48679122af89d5a7e00157e1908c12892edb35e6
Marianna <20104120@mail.wit.ie>
2025-07-29 22:05:36 +0100

    clean main.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 import os
+
+
 
 load_dotenv()
 
@@ -23,3 +28,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(
+#             host=os.getenv("DB_HOST"),
+#             database=os.getenv("DB_NAME"),
+#             user=os.getenv("DB_USER"),
+#             password=os.getenv("DB_PASSWORD"), 
+#             cursor_factory=RealDictCursor,
+#         )
+#         cursor = conn.cursor()
+#         print("✅ Database connection was successful")
+#         break
+#     except Exception as error:
+#         print("❌ Connecting to the database failed")
+#         print("Error:", error)
+#         time.sleep(2)
+
--- app/main.py
+++ app/main.py
@@ -1,55 +1,12 @@
-from typing import Optional, List
-
-
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel
-from fastapi.params import Body
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from sqlalchemy.sql.functions import mode
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
-from dotenv import load_dotenv
-import os
 
-load_dotenv() 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"), 
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("✅ Database connection was successful")
-        break
-    except Exception as error:
-        print("❌ Connecting to the database failed")
-        print("Error:", error)
-        time.sleep(2)
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
 
 app.include_router(post.router)
 app.include_router(user.router)
