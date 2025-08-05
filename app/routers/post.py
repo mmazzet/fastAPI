d1b652c0845ab88a4de3a64ef026a036cfd8e287
Marianna <20104120@mail.wit.ie>
2025-08-05 22:34:49 +0100

    get multiple posts with join

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,25 +2,44 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from typing import List, Optional
 from .. import models, schemas
 from sqlalchemy.orm import Session
-from .. database import get_db
+from ..database import get_db
 from .. import oauth2
+from sqlalchemy import func
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
-@router.get("/", response_model=List[schemas.Post])
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(
-    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
     limit: int = 10,
     skip: int = 0,
-    search: Optional[str] = ""
+    search: Optional[str] = "",
 ):
-
 
     # Enables the owner to see only own posts
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
 
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = (
+    #     db.query(models.Post)
+    #     .filter(models.Post.title.contains(search))
+    #     .limit(limit)
+    #     .offset(skip)
+    #     .all()
+    # )
+
+    posts = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.title.contains(search))
+        .limit(limit)
+        .offset(skip)
+        .all()
+    )
+
     return posts
 
 
@@ -58,14 +77,13 @@ def get_post(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {id} was not found",
         )
-    
+
     # Enables only the owner of the posts to get one of own posts
     # if post.owner_id != current_user.id:
     #     raise HTTPException(
     #         status_code=status.HTTP_403_FORBIDDEN,
     #         detail="Not authorized to perform requested action",
     #     )
-
 
     return post
 
--- app/schemas.py
+++ app/schemas.py
@@ -3,13 +3,16 @@ from datetime import datetime
 from typing import Optional
 from pydantic.types import conint
 
+
 class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
 
+
 class PostCreate(PostBase):
     pass
+
 
 class UserOut(BaseModel):
     id: int
@@ -19,15 +22,24 @@ class UserOut(BaseModel):
     class Config:
         orm_mode = True
 
+
 class Post(PostBase):
     id: int
     created_at: datetime
     owner_id: int
     owner: UserOut
-    
 
     class Config:
         orm_mode = True
+
+
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True
+
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -38,9 +50,11 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 class TokenData(BaseModel):
     id: Optional[str]
