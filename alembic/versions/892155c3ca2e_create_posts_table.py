9426bd43ddfad64adc00e056dc46cd41c9e59ebc
Marianna <20104120@mail.wit.ie>
2025-08-06 22:29:45 +0100

    introduce alembic

---
+++ alembic/versions/892155c3ca2e_create_posts_table.py
@@ -0,0 +1,33 @@
+"""create posts table
+
+Revision ID: 892155c3ca2e
+Revises:
+Create Date: 2025-08-06 22:10:25.993427
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "892155c3ca2e"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "posts", sa.Column("id", sa.Integer(), nullable=False, primary_key=True), sa.Column('title', sa.String(), nullable=False)
+    )
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('posts')
+    pass
