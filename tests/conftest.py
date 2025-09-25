3b2adf583031cc2649f2e92131a1d0bf6f16a5d4
Marianna <20104120@mail.wit.ie>
2025-09-25 21:20:47 +0100

    create conftest and moved test_user

---
+++ tests/conftest.py
@@ -0,0 +1,65 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.database import get_db, Base
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/fastapi_test"
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:somepsw@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base.metadata.create_all(bind=engine)
+
+
+
+# def override_get_db():
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+
+# client = TestClient(app)
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email":"pippa@user.com", "password":"secret"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,9 @@
 import pytest
 from jose import jwt
 from app import schemas
-from .database import client, session
 from app.config import settings
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email":"pippa@user.com", "password":"secret"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    print(res.json())
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
+
 
 # def test_root(client):
 #     res = client.get("/")
