827ab59e4d801511200883f5aff4bdbde2dae807
Marianna <20104120@mail.wit.ie>
2025-09-27 17:07:03 +0100

    add get_all_posts and fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db, Base
 from app.oauth2 import create_access_token
+from app import models
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/fastapi_test"
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:somepsw@localhost:5432/fastapi_test'
@@ -76,3 +77,36 @@ def authorized_client(client, token):
         "Authorization": f"Bearer {token}"
     }
     return client
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id":test_user['id']
+    },{
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id":test_user['id']
+    },
+    {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id":test_user['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+
+    posts = list(post_map)
+
+    session.add_all(posts)
+    # session.add_all([models.Post(title='first title', content="first content", owner_id=test_user['id']),
+    #                 models.Post(title='2nd title', content="2nd content", owner_id=test_user['id']),
+    #                 models.Post(title='3rd title', content="3rd content", owner_id=test_user['id'])
+    #                 ])
+    session.commit()
+    posts = session.query(models.Post).all()
+    return posts
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,6 +1,6 @@
 
 
-def test_get_all_posts(authorized_client):
+def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
     print(res.json())
     assert res.status_code == 200
