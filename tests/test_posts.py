f3e1c4b6c36706df87f155da2d90135d1678a4f0
Marianna <20104120@mail.wit.ie>
2025-09-28 21:48:04 +0100

    add tests unauthorized_user_get all and one posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,6 +1,20 @@
-
+from typing import List
+from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    print(res.json())
+    def validate(post):
+        return schemas.PostOut(**post)
+    post_map = map(validate, res.json())
+    # print(list(post_map))
+    # posts_list = list(post_map)
+    assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
